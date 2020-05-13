a82db3dcf8561004c06744e0271bf429d3624e7c
hustquick <hustquick@gmail.com>
2020-05-13 13:38:36 +0800

    增加了注释。

--- Field.py
+++ Field.py
@@ -9,17 +9,20 @@ import datetime
 import geocoder
 
 
+# 将向量单位化
 def uni_vector(v):
     length = np.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
     return v/length
 
 
+# 求两向量的角平分线向量
 def angular_bisector_vector(v_1, v_2):
     v1_uni = uni_vector(v_1)
     v2_uni = uni_vector(v_2)
     return uni_vector(v1_uni + v2_uni)
 
 
+# 将向量的高度角和方位角形式改写成坐标形式
 def angle2vector(alt, azi):
     angle1 = np.pi/2 - np.deg2rad(alt)
     x = np.sin(angle1)*np.sin(np.deg2rad(azi))
@@ -28,19 +31,14 @@ def angle2vector(alt, azi):
     return np.array([x, y, z])
 
 
-def vector2angle(a, b, c):
-    d = np.sqrt(a**2 + b**2)
-    alt = np.arctan2(c, d)
-    azi = np.pi/2 - np.arctan2(b, a)
-    return alt, azi
-
-
+# 求两个向量之间的夹角的余弦值
 def cosine(v_1, v_2):
     uni_v_1 = uni_vector(v_1)
     uni_v_2 = uni_vector(v_2)
     return uni_v_1.dot(uni_v_2)
 
 
+# 根据向量旋转前的位置和旋转后的位置，找出其旋转轴和旋转角（右手方向）
 def calculate_rotation(vector_before, vector_after):
     rotation_axis_ = np.cross(vector_before, vector_after)
     rot_axis = uni_vector(rotation_axis_)
@@ -48,6 +46,7 @@ def calculate_rotation(vector_before, vector_after):
     return rot_axis, rot_angle
 
 
+# 求解绕旋转轴unit_rotation_axis旋转rotation_angle角度时的旋转矩阵
 def axis_rotation_matrix(unit_rotation_axis, rotation_angle):
     x = unit_rotation_axis[0]
     y = unit_rotation_axis[1]
@@ -67,6 +66,7 @@ def axis_rotation_matrix(unit_rotation_axis, rotation_angle):
     return T
 
 
+# 求解一个点在另一个坐标系中的坐标值。该坐标系给定了z轴的方向，x轴位于原坐标系x0y0平面上。
 def coordinate_rotation_matrix(point, vector_z):
     """
     vector_x is defined in the xy plane
@@ -77,16 +77,15 @@ def coordinate_rotation_matrix(point, vector_z):
     x1, x2, x3 = vector_x
     y1, y2, y3 = vector_y
     R = np.array([
-        [x1, y1, z1,  0],
-        [x2, y2, z2,  0],
-        [x3, y3, z3,  0],
-        [ 0,  0,  0,  1],
+        [x1, y1, z1],
+        [x2, y2, z2],
+        [x3, y3, z3],
                  ])
     p1, p2, p3 = point
-    p4 = np.array([p1, p2, p3, 1]).reshape(-1, 1)
+    p4 = np.array([p1, p2, p3]).T
     p4_p = np.dot(R, p4)
-    point_p = p4_p[:][0:3]
-    return point_p.ravel()
+    point_p = p4_p.ravel()
+    return point_p
 
 
 def alpha(distance):
@@ -96,6 +95,7 @@ def alpha(distance):
         return np.exp(-0.0001106*distance)
 
 
+# 获取当前IP地址的经纬度
 def get_lalo():
     g = geocoder.ip('me')
     latlng = g.latlng
@@ -105,7 +105,7 @@ def get_lalo():
     return latlng
 
 
-mirror_typical = Mirror()
+mirror_typical = Mirror()   # 取一面典型的镜子，获取其参数值。
 mirror_length = mirror_typical.length
 mirror_center_height = mirror_typical.center_height
 tower_height = 115
@@ -113,27 +113,27 @@ min_distance = 11.6  # Should be larger than mirror_length * sqrt(2)
 base_radius = 115
 number_of_rings = 16
 
-# 1代表各镜面坐标系，2代表塔坐标系（世界坐标系），3代表光线坐标系
+# 1代表各镜面坐标系，2代表塔坐标系（世界坐标系，该坐标系中取正东为x轴正方向，正北为y轴正方向，竖直向上为z轴正方向），
+# 3代表光线坐标系
 v1 = np.array([0, 0, 1])     # 镜面法线向量在镜面坐标系中的坐标值
 # 计算太阳高度角
-lalo = [37.22, 97.23]   # 手动指定纬度和经度，也可以注释改行，反注释下行，自动获取当前位置的经纬度进行计算
+lalo = [37.22, 97.23]   # 手动指定纬度和经度，也可以注释该行，反注释下行，自动获取当前位置的经纬度进行计算
 # lalo = get_lalo()     # 计算当前经纬度
 observer = ephem.Observer()
 observer.lat, observer.lon = str(lalo[0]), str(lalo[1])
 
-observer.date = ephem.Date('2020/03/20 06:00:00.00')  # 手动指定时间(UTC时间)，也可以注释改行，反注释下面两行，自动获取当前时间进行计算
+observer.date = ephem.Date('2020/03/20 06:00:00.00')  # 手动指定时间(UTC时间)，也可以注释该行，反注释下面两行，自动获取当前时间进行计算
 # gatech.date = datetime.datetime.utcnow()    # 计算当前时间
 # print("\n当前时间为:\n%s" % ephem.localtime(gatech.date))
-
 
 sun = ephem.Sun()
 sun.compute(observer)
 
 print("太阳高度角: %s \n太阳方位角: %s" % (sun.alt, sun.az))
-# altitude = sun.alt
-# azimuth = sun.az
-altitude = 52.78
-azimuth = 180
+altitude = np.rad2deg(sun.alt)  # 也可以注释该两行，反注释下面两行，手动输入需要计算的高度角和方位角
+azimuth = np.rad2deg(sun.az)
+# altitude = 52.78
+# azimuth = 180
 v3 = angle2vector(altitude, azimuth)    # 光线向量在世界坐标系中的坐标值，指向太阳！
 
 radius = np.zeros(number_of_rings)      # 用来记录各圈的半径
